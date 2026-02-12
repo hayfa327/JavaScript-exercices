2aea29a6b2504e1daba2f2aae6fc2a33a4063e6e
hayfa safa <haifasafaa@gmail.com>
2026-02-12 22:46:05 +0100

    question ### partA

--- Exercises/test.js
+++ Exercises/test.js
@@ -1,27 +1,5 @@
-function createCounter (startValue = 0 )  {
-    let count = startValue; 
-    return {
-      increment() {
-      count++; 
-      return count; 
-    }, 
-   
-    decrement() {
-      count--;
-      return count;
-    },
-
-    getCount() {
-      return count;
-    }
-  }; 
+for (var i = 1; i <= 3; i++) {
+        console.log(i);
+    setTimeout (() => {( i * 1000);
+      
 } 
-
-
-const counter = createCounter(5);
-createCounter(); 
-console.log(counter.getCount());  // 5
-console.log(counter.increment()); // 6
-console.log(counter.increment()); // 7
-console.log(counter.decrement()); // 6
-console.log(counter.getCount());
